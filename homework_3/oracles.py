c87f46ddeb32e12aebd894d32a5e2007302f4132
Alexander Rogozin <aleksandr.rogozin@phystech.edu>
2020-03-28 22:31:46 +0500

    Initial commit

---
+++ homework_3/oracles.py
@@ -0,0 +1,47 @@
+class BaseSmoothOracle(object):
+    """
+    Base class for implementation of oracles.
+    """
+    def func(self, x):
+        """
+        Computes the value of function at point x.
+        """
+        raise NotImplementedError('Func oracle is not implemented.')
+
+    def grad(self, x):
+        """
+        Computes the gradient at point x.
+        """
+        raise NotImplementedError('Grad oracle is not implemented.')
+    
+    def func_directional(self, x, d, alpha):
+        """
+        Computes phi(alpha) = f(x + alpha*d).
+        """
+        return np.squeeze(self.func(x + alpha * d))
+
+    def grad_directional(self, x, d, alpha):
+        """
+        Computes phi'(alpha) = (f(x + alpha*d))'_{alpha}
+        """
+        return np.squeeze(self.grad(x + alpha * d).dot(d))
+
+
+class QuadraticOracle(BaseSmoothOracle):
+    """
+    Oracle for quadratic function:
+       func(x) = 1/2 x^TAx - b^Tx.
+    """
+    
+    def __init__(self, A, b):
+        if not scipy.sparse.isspmatrix_dia(A) and not np.allclose(A, A.T):
+            raise ValueError('A should be a symmetric matrix.')
+        self.A = A
+        self.b = b
+
+    def func(self, x):
+        # your code here
+
+    def grad(self, x):
+        # your code
+
