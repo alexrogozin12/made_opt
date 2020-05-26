d21a732ae26d9b58c4ab7c80f1227255caebd819
alex_rogozin12 <aleksandr.rogozin@phystech.edu>
2020-05-26 21:33:52 +0500

    add QuadraticOracle

--- homework_6/oracles.py
+++ homework_6/oracles.py
@@ -90,3 +90,20 @@ def create_log_reg_oracle(A, b, regcoef):
         return A.transpose() * sA
 
     return LogRegL2Oracle(matvec_Ax, matvec_ATx, matmat_ATsA, b, regcoef)
+
+
+class QuadraticOracle(BaseSmoothOracle):
+    def __init__(self, A, b):
+        if not scipy.sparse.isspmatrix_dia(A) and not np.allclose(A, A.T):
+            raise ValueError('A should be a symmetric matrix.')
+        self.A = A
+        self.b = b
+
+    def func(self, x):
+        return 0.5 * np.dot(self.A.dot(x), x) - self.b.dot(x)
+
+    def grad(self, x):
+        return self.A.dot(x) - self.b
+
+    def hess(self, x):
+        return self.A
